ebc72e04fcd4944a1f8d6436ec0d9211b7ec3133
muhammad nosahd <noshadkhan997788@gmail.com>
2024-10-22 13:09:36 -0700

    first commit

---
+++ step00_helloworld/app.ts
@@ -0,0 +1,4 @@
+// let' write a simple Helloworld! in typescript
+console.log("helloworld") // helloworld
+// open cmd & write tsc ___.ts   > create js file automatically  then,
+// type node ____.js     > show the output of your code!
---
+++ step01_variables/app.ts
@@ -0,0 +1,26 @@
+// In TypeScript, variables are used to store and manage data
+// In TypeScript, variables are declared using the let, const, or var keywords.
+
+// // 01 var re-assigned & re-declred
+
+// // var Re-declare
+// var myname1 : string = "muhammmad noshad";
+// var myname1 : string = "muhammad anwar khan";
+// /// var Re-assigned
+// myname1 = "muhammad noshad";
+// console.log(myname1); // muhammad anwar khan
+
+// // 02 let re-declare but not re-assigned
+
+// // let Re-declare
+// let myname2 : string = "muhammad noshad";
+// myname2 = "muhammad anwar khan";
+// console.log(myname2); // muhammad anwarr khan
+// // let Re-assigned
+// // let myname2 = "muhammad noshad"; // error
+
+// // 03 const not re-declare & not re-assigned
+// const myname3 : string = "muhammad nnoshad";
+// //const myname3 : string = "muhammmad noshahd khan"; // error
+// //myname3 = "muhammad anwar khan" // error
+// console.log(myname3);
